f0eae0c34d9891273814318eaac9f2868a3baeb6
unknown <krishnadileep28@gmail.com>
2025-04-10 02:11:46 +0100

    Events page Completed

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('events/', views.events_view, name='events'),
+    path("add-event/", views.add_event, name="add_event"),
+    path('register-event/', views.register_event, name='register_event'),
+    path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
 ]
